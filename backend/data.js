81d5604ca12c109d5d679b392658782a867d0363
MikelEngel <angelhevel@gmail.com>
2022-12-26 20:34:05 -0600

    axios install & use

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'LONA DE LENO',
+      slug: 'lona-de-leno',
+      category: 'LONAS',
+      image: '/images/lona.jpg', //679 x 829
+      price: 120,
+      brand: 'Aoking',
+      description: 'Lona de leno uso pesado',
+    },
+    {
+      name: 'RUEDA PARA DIABLO',
+      slug: 'rueda-para-diablo',
+      category: 'RUEDAS',
+      image: '/images/rueda.jpg',
+      price: 120,
+      brand: 'HOD',
+      description: 'Rueda tipo rayo 12x3',
+    },
+    {
+      name: 'RODAJA FIJA',
+      slug: 'rodaja-fija',
+      category: 'RODAJAS',
+      image: '/images/rodaja.jpg',
+      price: 120,
+      brand: 'HOD',
+      description: 'Rodaja fija uso pesado',
+    },
+    {
+      name: 'ESTANTERÍA DE ALAMBRE',
+      slug: 'estanteria-de-alambre',
+      category: 'ESTANTERÍA',
+      image: '/images/estante.jpg',
+      price: 120,
+      brand: 'HOD',
+      description: 'Estantería de alambre multiusos',
+    },
+  ],
+};
+
+export default data;
